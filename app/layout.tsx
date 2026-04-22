219d59ac9d333b61a73c670571f70584b5c829aa
Péter Ákos <peterakos19@gmail.com>
2026-04-22 16:13:30 +0300

    fix: remove coming soon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,12 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body className={`${inter.variable} antialiased`}>
-        <div className="mobile-coming-soon" aria-live="polite">
-          Coming soon.
-        </div>
-        <div className="desktop-site-content">{children}</div>
-      </body>
+      <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
   );
 }
